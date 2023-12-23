4177675deee4b592554d29fc553a4c310ba7c5aa
chandan satwani <101943277+chandan2311@users.noreply.github.com>
2023-12-23 07:22:22 +0530

    yes

---
+++ rellocatable.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 20
+
+struct estab
+{
+    char csname[10];
+    char extsym[10];
+    int address;
+    int length;
+} es[MAX];
+
+int main()
+{
+    char input[10], name[10];
+    int count = 0, progaddr, csaddr, add, len;
+    FILE *fp1, *fp2;
+
+    fp1 = fopen("linkinput.txt", "r");
+    fp2 = fopen("loadmap.txt", "w");
+
+    printf("Enter the location where the program has to be loaded : ");
+    scanf("%d", &progaddr);
+
+    csaddr = progaddr;
+
+    fprintf(fp2, "CS_NAME\tEXT_SYM_NAME\tADDRESS\tLENGTH\n");
+    fprintf(fp2, "--------------------------------------\n");
+
+    fscanf(fp1, "%s", input);
+
+    while (strcmp(input, "END") != 0)
+    {
+        if (strcmp(input, "H") == 0)
+        {
+            fscanf(fp1, "%s", name);
+            strcpy(es[count].csname, name);
+            strcpy(es[count].extsym, "**");
+            fscanf(fp1, "%d", &add);
+            es[count].address = add + csaddr;
+            fscanf(fp1, "%d", &len);
+            es[count].length = len;
+            fprintf(fp2, "%s\t%s\t\t%d\t%d\n", es[count].csname, es[count].extsym, es[count].address, es[count].length);
+            count++;
+        }
+        else if (strcmp(input, "D") == 0)
+        {
+            fscanf(fp1, "%s", input);
+            while (strcmp(input, "R") != 0)
+            {
+                strcpy(es[count].csname, "**");
+                strcpy(es[count].extsym, input);
+                fscanf(fp1, "%d", &add);
+                es[count].address = add + csaddr;
+                es[count].length = 0;
+                fprintf(fp2, "%s\t%s\t\t%d\t%d\n", es[count].csname, es[count].extsym, es[count].address, es[count].length);
+                printf("%s\t%s\t\t%d\t%d\n", es[count].csname, es[count].extsym, es[count].address, es[count].length);
+                count++;
+                fscanf(fp1, "%s", input);
+            }
+            csaddr = csaddr + len;
+        }
+        else if (strcmp(input, "T") == 0)
+        {
+            while (strcmp(input, "E") != 0)
+                fscanf(fp1, "%s", input);
+        }
+        fscanf(fp1, "%s", input);
+    }
+
+    fprintf(fp2, "--------------------------------------\n");
+
+    fclose(fp1);
+    fclose(fp2);
+
+    printf("FINISHED\n");
+
+    return 0;
+}
